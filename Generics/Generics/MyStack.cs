0c85165cbc9202005096b03a4e212cadabe89fc8
Omprakash Narayanan <omprakashnarayanan302@gmail.com>
2023-04-24 14:52:48 -0400

    Commit for MyStack

---
+++ Generics/Generics/MyStack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class MyStack<T>
+    {
+        List<T> stack = new List<T>();
+        int count = 0;
+
+        public void Push(T Element)
+        {
+            this.stack.Add(Element);
+            count = count + 1;
+        }
+
+        public T Pop()
+        {
+            T RemovedElement;
+            T temp = default(T);
+
+            if (count > 0)
+            {
+                RemovedElement = stack[count - 1];
+                count--;
+                return RemovedElement;
+            }
+            return temp;
+        }
+
+        public int Count { get { return count; } }
+    }
+}
---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,18 @@
+﻿using Generics;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack<int> stk = new MyStack<int>();
+            stk.Push(1);
+            stk.Push(2);
+            stk.Push(3);
+            Console.WriteLine(stk.Count);
+            stk.Pop();
+            Console.WriteLine(stk.Count);
+        }
+    }
+}
